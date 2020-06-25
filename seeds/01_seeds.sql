be61e3e448f5b77b4b7b69a191a35e5cbe181e91
Kevin Kim <kevinfkim@gmail.com>
2020-06-25 02:03:19 +0000

    typo in VALUE, should be VALUES. oops.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,23 +9,23 @@
 --------------------|
 -- property_reviews | id, guest_id, property_id, reservation_id, rating, message
 
-INSERT INTO users (name, email password)
-VALUE ('Kevin Kim', 'kevin@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-      ('Joanne Fung', 'joanne@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-      ('Leona Kim', 'leona@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password)
+VALUES ('Kevin Kim', 'kevin@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('Joanne Fung', 'joanne@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('Leona Kim', 'leona@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUE (1, 'Porter', 'A description for Porter', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 900, 2, 3, 3, 'Canada', 'Porter Ave', 'Vaughan', 'Ontario', 'A1B2C3', true),
-      (1, 'Shemer', 'A description for Shemer', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 1000, 4, 4, 3, 'Canada', 'Shemer Dr', 'Vaughan', 'Ontario', 'B2C3D4', false),
-      (2, 'Hiking', 'A description for Hiking', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 1500, 8, 6, 5, 'Canada', 'Hiking Crt', 'Brampton', 'Ontario', 'C3D4E5', true);
+VALUES (1, 'Porter', 'A description for Porter', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 900, 2, 3, 3, 'Canada', 'Porter Ave', 'Vaughan', 'Ontario', 'A1B2C3', true),
+       (1, 'Shemer', 'A description for Shemer', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 1000, 4, 4, 3, 'Canada', 'Shemer Dr', 'Vaughan', 'Ontario', 'B2C3D4', false),
+       (2, 'Hiking', 'A description for Hiking', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 1500, 8, 6, 5, 'Canada', 'Hiking Crt', 'Brampton', 'Ontario', 'C3D4E5', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUE (2020-01-01, 2020-02-01, 3, 3),
-      (2019-02-03, 2019-03-04, 2, 3),
-      (2018-03-04, 2019-04-05, 1, 3);
+VALUES (2020-01-01, 2020-02-01, 3, 3),
+       (2019-02-03, 2019-03-04, 2, 3),
+       (2018-03-04, 2019-04-05, 1, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUE (3, 1, 3, 5, 'A message for Porter'),
-      (3, 2, 2, 5, 'A message for Porter'),
-      (3, 3, 1, 5, 'A message for Porter');
+VALUES (3, 1, 3, 5, 'A message for Porter'),
+       (3, 2, 2, 5, 'A message for Porter'),
+       (3, 3, 1, 5, 'A message for Porter');
 
