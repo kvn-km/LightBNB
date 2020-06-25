48d86e12a703a961c6cf8783741179afb5d1128d
Kevin Kim <kevinfkim@gmail.com>
2020-06-25 02:00:17 +0000

    created script for INSERT values into the four tables. 01_seeds

--- migrations/00_trial_schema.sql
+++ migrations/00_trial_schema.sql
@@ -1,58 +1,42 @@
-CREATE TABLE addresses
-(
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id),
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  cost_per_night INT NOT NULL,
+  parking_spaces INT,
+  number_of_bathrooms INT,
+  number_of_bedrooms INT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
   country VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
-  post_code VARCHAR(255) NOT NULL
+  post_code VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT true
+  );
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INT REFERENCES users(id) NOT NULL,
+  property_id INT REFERENCES properties(id) NOT NULL,
+  start_date DATE,
+  end_date DATE
 );
 
-CREATE TABLE guest
-(
+CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE owners
-(
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE properties
-(
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
-  cost_per_night INTEGER NOT NULL,
-  parking_spaces INTEGER,
-  number_of_bathrooms INTEGER,
-  number_of_bedrooms INTEGER,
-  photo_url VARCHAR(255),
-  is_active BOOLEAN DEFAULT t,
-  owner_id INTEGER REFERENCES owners(id) NOT NULL,
-  address_id INTEGER REFERENCES addresses(id) NOT NULL
-);
-
-CREATE TABLE reservations
-(
-  id SERIAL PRIMARY KEY NOT NULL,
-  start_date date,
-  end_date date,
-  guest_id INTEGER REFERENCES guests(id) NOT NULL,
-  property_id INTEGER REFERENCES properties(id) NOT NULL
-);
-
-CREATE TABLE reviews
-(
-  id SERIAL PRIMARY KEY NOT NULL,
-  rating INTEGER NOT NULL,
-  message VARCHAR(255),
-  guest_id INTEGER REFERENCES guests(id) NOT NULL,
-  property_id INTEGER REFERENCES properties(id) NOT NULL
+  guest_id INT REFERENCES users(id) NOT NULL,
+  property_id INT REFERENCES properties(id) NOT NULL,
+  reservation_id INT REFERENCES reservations(id) NOT NULL,
+  rating SMALLINT NOT NULL,
+  message VARCHAR(255)
 );
--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,48 +1,5 @@
 DROP TABLE IF EXISTS property_reviews, reservations, properties, users CASCADE;
 
--- CREATE TABLE users (
---   id SERIAL PRIMARY KEY NOT NULL,
---   name VARCHAR(255) NOT NULL,
---   email VARCHAR(255) NOT NULL,
---   password VARCHAR(255) NOT NULL
--- );
-
--- CREATE TABLE properties (
---   id SERIAL PRIMARY KEY NOT NULL,
---   owner_id INTEGER REFERENCES users(id),
---   title VARCHAR(255) NOT NULL,
---   description TEXT,
---   cost_per_night INT NOT NULL,
---   parking_spaces INT,
---   number_of_bathrooms INT,
---   number_of_bedrooms INT,
---   thumbnail_photo_url VARCHAR(255),
---   cover_photo_url VARCHAR(255),
---   country VARCHAR(255) NOT NULL,
---   province VARCHAR(255) NOT NULL,
---   city VARCHAR(255) NOT NULL,
---   street VARCHAR(255) NOT NULL,
---   post_code VARCHAR(255) NOT NULL,
---   is_active BOOLEAN DEFAULT true
---   );
-
--- CREATE TABLE reservations (
---   id SERIAL PRIMARY KEY NOT NULL,
---   guest_id INT REFERENCES users(id) NOT NULL,
---   property_id INT REFERENCES properties(id) NOT NULL,
---   start_date DATE,
---   end_date DATE
--- );
-
--- CREATE TABLE property_reviews (
---   id SERIAL PRIMARY KEY NOT NULL,
---   guest_id INT REFERENCES users(id) NOT NULL,
---   property_id INT REFERENCES properties(id) NOT NULL,
---   reservation_id INT REFERENCES reservations(id) NOT NULL,
---   rating SMALLINT NOT NULL,
---   message VARCHAR(255)
--- );
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -53,7 +10,6 @@ CREATE TABLE users (
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -62,13 +18,11 @@ CREATE TABLE properties (
   parking_spaces INTEGER  NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
-
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
