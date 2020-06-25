990bc6e8709fce07d3dc803a649d5aed1530abef
Kevin Kim <kevinfkim@gmail.com>
2020-06-25 02:21:26 +0000

    created a script to INSERT 3 rows in each table. It will REMOVE ALL CURRENT ROWS and then repopulate with 3 new ones in each.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,6 +9,11 @@
 --------------------|
 -- property_reviews | id, guest_id, property_id, reservation_id, rating, message
 
+DELETE FROM users;
+DELETE FROM properties;
+DELETE FROM reservations;
+DELETE FROM property_reviews;
+
 INSERT INTO users (name, email, password)
 VALUES ('Kevin Kim', 'kevin@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
        ('Joanne Fung', 'joanne@kvnkm.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
@@ -20,12 +25,12 @@ VALUES (1, 'Porter', 'A description for Porter', 'https://www.pexels.com/photo/w
        (2, 'Hiking', 'A description for Hiking', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 'https://www.pexels.com/photo/woman-wearing-orange-sweater-2613260/', 1500, 8, 6, 5, 'Canada', 'Hiking Crt', 'Brampton', 'Ontario', 'C3D4E5', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES (2020-01-01, 2020-02-01, 3, 3),
-       (2019-02-03, 2019-03-04, 2, 3),
-       (2018-03-04, 2019-04-05, 1, 3);
+VALUES ('2020-01-01', '2020-02-01', 3, 3),
+       ('2019-02-03', '2019-03-04', 2, 3),
+       ('2018-03-04', '2019-04-05', 1, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (3, 1, 3, 5, 'A message for Porter'),
-       (3, 2, 2, 5, 'A message for Porter'),
-       (3, 3, 1, 5, 'A message for Porter');
+       (3, 2, 2, 5, 'A message for Shemer'),
+       (3, 3, 1, 5, 'A message for Hiking');
 
